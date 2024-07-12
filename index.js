e2bf18287d43fea63d3fcd36db92f64f6059b476
05staff <139808462+05staff@users.noreply.github.com>
2024-07-12 20:45:23 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["LISTENNING TO","/website TO SEE OUR OFFICIAL WEBSITE"];
 
 
 let currentIndex = 0;
